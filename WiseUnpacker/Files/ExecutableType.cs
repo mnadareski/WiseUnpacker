1d2a1ec15116a30312e69ac00e72b2cba820adf9
Matt Nadareski <mnadareski@outlook.com>
2024-03-24 21:12:36 -0400

    Add summaries to enum

--- WiseUnpacker/Files/ExecutableType.cs
+++ WiseUnpacker/Files/ExecutableType.cs
@@ -3,7 +3,15 @@
     internal enum ExecutableType
     {
         Unknown,
+
+        /// <summary>
+        /// New Executable (NE) format
+        /// </summary>
         NE,
+
+        /// <summary>
+        /// Portable Executable (PE) format
+        /// </summary>
         PE,
     }
 }
