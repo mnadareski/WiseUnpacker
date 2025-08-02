9e7c76d6dd38d35d8e6605f0b4bd61a9325fcbce
Matt Nadareski <mnadareski@outlook.com>
2025-08-01 20:49:01 -0400

    Simplify output directory code

--- Test/Program.cs
+++ Test/Program.cs
@@ -15,23 +15,20 @@ namespace Test
                 return;
             }
 
-            // Get the input path and generate a default output
+            // Get the input path and optional output directory
             string input = args[0];
-            string? outdir = CreateOutdir(input);
+            string? outdir = args.Length > 1 ? args[1] : null;
+
+            // Generate an output directory
+            outdir = GenerateOutdir(input, outdir);
             if (string.IsNullOrEmpty(outdir))
             {
                 DisplayHelp("Could not determine output path");
                 return;
             }
 
-            // Use the provided output directory, if possible
-            if (args.Length > 1)
-                outdir = Path.GetFullPath(args[1]);
-
-            // Create a new unpacker
-            var unpacker = new Unpacker(input);
-
             // Attempt to extract the file
+            var unpacker = new Unpacker(input);
             if (unpacker.Run(outdir!))
                 Console.WriteLine($"Extracted {input} to {outdir}");
             else
@@ -47,42 +44,44 @@ namespace Test
             if (!string.IsNullOrEmpty(err))
                 Console.WriteLine($"Error: {err}");
 
-            Console.WriteLine(@"Usage: Test <input> [output]
-
-<input> is required and must be the path to the input file.
-Only one path can be specified at a time.
-
-[output] is optional and represents an output folder for extracted files.
-If not specified, a folder will be generated next to the input file.");
+            Console.WriteLine("Usage: Test <input> [output]");
+            Console.WriteLine();
+            Console.WriteLine("<input> is required and must be the path to the input file.");
+            Console.WriteLine("Only one path can be specified at a time.");
+            Console.WriteLine();
+            Console.WriteLine("[output] is optional and represents an output folder for extracted files.");
+            Console.WriteLine("If not specified, a folder will be generated next to the input file.");
         }
 
         /// <summary>
         /// Generate the output directory path, if possible
         /// </summary>
         /// <param name="input">Input path to generate from</param>
+        /// <param name="outdir">User provided output directory</param>
         /// <returns>Output directory path on success, null on error</returns>
-        private static string? CreateOutdir(string input)
+        private static string? GenerateOutdir(string input, string? outdir)
         {
             // If the file path is not valid
             if (string.IsNullOrEmpty(input) || !File.Exists(input))
                 return null;
+
+            // Use the provided output directory, if possible
+            if (outdir != null)
+                return Path.GetFullPath(outdir);
 
             // Get the full path for the input, if possible
             input = Path.GetFullPath(input);
 
             // Get the directory name and filename without extension for processing
             string? directoryName = Path.GetDirectoryName(input);
-            string? fileNameWithoutExtension = Path.GetFileNameWithoutExtension(input);
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(input);
 
             // Return an output path based on the two parts
-            if (string.IsNullOrEmpty(directoryName) && string.IsNullOrEmpty(fileNameWithoutExtension))
-                return null;
-            else if (string.IsNullOrEmpty(directoryName) && !string.IsNullOrEmpty(fileNameWithoutExtension))
-                return fileNameWithoutExtension;
-            else if (!string.IsNullOrEmpty(directoryName) && string.IsNullOrEmpty(fileNameWithoutExtension))
-                return directoryName;
-            else
-                return Path.Combine(directoryName!, fileNameWithoutExtension!);
+            outdir = fileNameWithoutExtension;
+            if (!string.IsNullOrEmpty(directoryName))
+                outdir = Path.Combine(directoryName, outdir);
+
+            return outdir;
         }
     }
 }
