95ec005c6d81745a5a59fc616f4880556d24c667
Matt Nadareski <mnadareski@outlook.com>
2025-08-05 16:04:24 -0400

    Add note about 0x06

--- WiseUnpacker/SabreTools.Models.WiseInstaller/ScriptUnknown0x06.cs
+++ WiseUnpacker/SabreTools.Models.WiseInstaller/ScriptUnknown0x06.cs
@@ -1,6 +1,14 @@
 namespace SabreTools.Models.WiseInstaller
 {
+    /// <remarks>
+    /// This is likely compressed Billboard data, though it is
+    /// very difficult to determine what the format of the extracted
+    /// data is. The official documentation mentions that it's
+    /// a vector format, but it does not appear to be natively
+    /// openable with standard graphics viewing programs.
+    /// </remarks>
     /// <see href="https://codeberg.org/CYBERDEV/REWise/src/branch/master/src/wisescript.h"/> 
+    /// <see href="https://www.manualslib.com/manual/404969/Symantec-Wisescript-Editor-8-0-Reference-For-Wise-Package-Studio-V1-0.html"/> 
     public class ScriptUnknown0x06 : MachineStateData
     {
         /// <summary>
