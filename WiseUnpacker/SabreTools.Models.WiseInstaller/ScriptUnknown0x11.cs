972c2accbe682f01405af59ca67c016babf0d63c
Matt Nadareski <mnadareski@outlook.com>
2025-08-05 15:05:53 -0400

    Better investigation notes on 0x11

--- WiseUnpacker/SabreTools.Models.WiseInstaller/ScriptUnknown0x11.cs
+++ WiseUnpacker/SabreTools.Models.WiseInstaller/ScriptUnknown0x11.cs
@@ -1,20 +1,13 @@
 namespace SabreTools.Models.WiseInstaller
 {
     /// <remarks>
-    /// Every instance of the string has been a .lnk file
-    /// with no apparent destination. It seems to be acting
-    /// like a check space available or a `touch` command,
-    /// since they mostly occur before a copy function.
-    /// 
-    /// There is one instance where it is doing a path with
-    /// a wildcard. This may mean this is a delete command.
-    /// The issue with this theory is that there do not seem
-    /// to be any flags, notably missing the "Include Sub Directories"
-    /// and "Remove Directory Containing Files" items.
-    /// 
-    /// There's already another definition for delete file,
-    /// so that just muddies the waters about what this is
-    /// supposed to be further.
+    /// This seems to indicate files that are ignored or otherwise
+    /// not copied from the source. It has been observed before
+    /// link files are created and in the middle of large chains
+    /// of install file calls. Unfortunately, the only instance
+    /// where this could be found in a source script does not give
+    /// any indication as to why the paths in question are being
+    /// ignored.
     /// </remarks>
     /// <see href="https://codeberg.org/CYBERDEV/REWise/src/branch/master/src/wisescript.h"/> 
     /// <see href="https://www.manualslib.com/manual/404969/Symantec-Wisescript-Editor-8-0-Reference-For-Wise-Package-Studio-V1-0.html"/> 
